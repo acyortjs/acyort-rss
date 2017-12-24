f489e4e7981c7aac52e8e0b598ca699104657086
LoeiFy <LoeiFy@gmail.com>
2017-12-24 19:38:26 +0800

    rss generator

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-const pathFn = require('path')
-const Rss = require('./rss')
+const generator = require('../../lib/generator')
 
-acyort.extend.register('after_build', (data) => {
-  const {
-    config,
-    fs,
-    logger,
-  } = acyort
-
-  if (!config.rss) {
-    return false
-  }
-
-  const { posts } = data
-  const { limit, path } = config.rss
-  const { base, public_dir } = config
-  const rss = new Rss({ ...config, limit })
-
-  fs.outputFileSync(pathFn.join(base, public_dir, path), rss.render(posts))
-
-  return logger.success(path)
-})
+acyort.extend.register('after_build', generator.bind(acyort))
